03214cb36d1c96db93f0ca286f95286075119bb5
Campbell Glass <campbelltmglass@gmail.com>
2016-09-10 23:39:18 -0700

    Setup sublist go package

---
+++ go/src/sublist/hello.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, world!")
+	fmt.Println("This is where we'll be running and testing the server side code.")
+	fmt.Println("Eventually we'll make it nice, but this is good enough for now.")
+	fmt.Println("The exciting part is that it runs!")
+}
