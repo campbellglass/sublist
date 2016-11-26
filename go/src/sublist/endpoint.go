04927d6399c8b8dc612ef3e022d4f97e22a46458
Campbell Glass <campbelltmglass@gmail.com>
2016-11-26 09:09:15 -0800

    NFC: Move endpoint creation from server.go to endpoint.go

--- go/src/sublist/endpoint.go
+++ go/src/sublist/endpoint.go
@@ -1,20 +1,57 @@
 package main
 
 import (
+	"fmt"
 	"regexp"
 
 	"github.com/valyala/fasthttp"
 )
 
+// Endpoint infrastructure
 type Endpoint struct {
 	Regexp  *regexp.Regexp
-	Handler func(*fasthttp.RequestCtx)
+	Handler func(*Server, *fasthttp.RequestCtx)
 }
 
-func NewEndpoint(rgx string, handler func(*fasthttp.RequestCtx)) *Endpoint {
+func NewEndpoint(rgx string,
+	handler func(*Server, *fasthttp.RequestCtx)) *Endpoint {
+
 	endpoint := Endpoint{
 		Regexp:  regexp.MustCompile(rgx),
 		Handler: handler,
 	}
 	return &endpoint
+}
+
+// Actual endpoint creation
+// Endpoints are ordered from most specific to least specific
+var (
+	nodes = NewEndpoint(`^\/nodes\/?$`, GetNodesHandler)
+	root  = NewEndpoint(`^\/$`, RootHandler)
+
+	// Order matters here - the first occurring match will be used
+	Endpoints = []*Endpoint{
+		nodes,
+		root,
+	}
+)
+
+// GetNodesHandler returns all nodes present
+func GetNodesHandler(server *Server, ctx *fasthttp.RequestCtx) {
+	nodes, err := server.db.GetNodes()
+	if err != nil {
+		ctx.Error(err.Error(), fasthttp.StatusInternalServerError)
+		return
+	}
+	defaultJSON, err := nodes.ToBytes()
+	if err != nil {
+		ctx.Error(err.Error(), fasthttp.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(ctx, "%s\n", defaultJSON)
+}
+
+// RootHandler returns a basic message
+func RootHandler(server *Server, ctx *fasthttp.RequestCtx) {
+	fmt.Fprintf(ctx, "This is the root path\n")
 }
--- go/src/sublist/server.go
+++ go/src/sublist/server.go
@@ -24,45 +24,16 @@ func (server *Server) Start() {
 
 // Route Routes API Requests to the appropriate handler function
 func (server *Server) Route(ctx *fasthttp.RequestCtx) {
-	// Route URI regexp
-	// Routes are ordered from most specific to least specific
-	nodes := NewEndpoint(`^\/nodes\/?$`, server.GetNodesHandler)
-	root := NewEndpoint(`^\/$`, server.RootHandler)
-
-	endpoints := []*Endpoint{
-		nodes,
-		root,
-	}
 
 	// Route matching
 	path := string(ctx.Path())
-	for _, endpoint := range endpoints {
+	for _, endpoint := range Endpoints {
 		if endpoint.Regexp.MatchString(path) {
-			endpoint.Handler(ctx)
+			endpoint.Handler(server, ctx)
 			return
 		}
 	}
 
 	// Default case
 	ctx.Error(fmt.Sprintf("Invalid path: '%s'", path), fasthttp.StatusNotFound)
-}
-
-// RootHandler returns a basic message
-func (server *Server) RootHandler(ctx *fasthttp.RequestCtx) {
-	fmt.Fprintf(ctx, "This is the root path\n")
-}
-
-// GetNodesHandler returns all nodes present
-func (server *Server) GetNodesHandler(ctx *fasthttp.RequestCtx) {
-	nodes, err := server.db.GetNodes()
-	if err != nil {
-		ctx.Error(err.Error(), fasthttp.StatusInternalServerError)
-		return
-	}
-	defaultJSON, err := nodes.ToBytes()
-	if err != nil {
-		ctx.Error(err.Error(), fasthttp.StatusInternalServerError)
-		return
-	}
-	fmt.Fprintf(ctx, "%s\n", defaultJSON)
 }
